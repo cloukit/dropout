58ba4316cd2f048930f707d161b925605ef2870c
clouless <github@gruenewaldt.net>
2018-01-05 15:41:07 +0100

    v1.5.3 story for programmatic trigger

--- src/demo/demo.module.ts
+++ src/demo/demo.module.ts
@@ -7,12 +7,14 @@ import demoStoriesSource from '../assets/demoStoriesSource.json';
 import { CloukitStoryModule, CloukitStoryService } from '@cloukit/story';
 import { Story00Component } from './stories/story-00-dropout-directions';
 import { Story01Component } from './stories/story-01-off-placed-trigger-element';
+import { Story02Component } from './stories/story-02-programmatic-trigger';
 
 @NgModule({
   declarations: [
     DemoComponent,
     Story00Component,
     Story01Component,
+    Story02Component,
   ],
   exports: [
     DemoComponent,
